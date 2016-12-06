fe0bcd18c8e3cb6f8a0849cf17b6fd373bb2a657
Taylor Steil <taylor@foxcubgames.com>
2016-12-06 00:06:25 -0500

    * fixed import settings on some sprites (disabled mipmaps and compression) * changed scene around so the particles and trees spawn in the bottom left and grow towards the top right * made overdraw sprites transparent with a tiny bit of staggering in their position, also moved and resized them so they aren’t 100% fullscreen * moved camera to 0,0 with 0 rotation so positioning is easier * added a tiny bit of random rotation to the trees * increased size of all the slider thumbs so they are easier to tap on mobile

---
+++ GpuStressTestUnity/Assets/Stress Test/Code/Rotater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+	public Vector3 _rot;
+	public bool _random;
+
+	Transform _t;
+
+	void Awake() {
+		_t = transform;
+		if (_random) {
+			_rot = new Vector3(_rot.x * Random.value, _rot.y * Random.value, _rot.z * Random.value);
+		}
+	}
+
+	void Update() {
+		_t.Rotate(_rot * Time.deltaTime, Space.Self);
+	}
+
+}
