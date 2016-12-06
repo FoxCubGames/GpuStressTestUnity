8666b57714c73315373fb072802af6d387c4a0f2
Taylor Steil <taylor@foxcubgames.com>
2016-12-06 18:34:23 -0500

    * made fps display averaged over 30 frames * moved cloud sprite (overdraw) to be the closest thing to the camera

--- GpuStressTestUnity/Assets/Stress Test/Code/FPSDisplay.cs
+++ GpuStressTestUnity/Assets/Stress Test/Code/FPSDisplay.cs
@@ -4,16 +4,22 @@ using UnityEngine.UI;
 public class FPSDisplay : MonoBehaviour {
 
 	public Text _fpsText;
-	float _deltaTime = 0.0f;
+
+	int _frames;
+	float _time;
 
 	void Update() {
-		_deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
-		UpdateText();
+		_frames++;
+		_time += Time.deltaTime;
+
+		if (_frames >= 30) {
+			UpdateText();
+			_frames = 0;
+			_time = 0f;
+		}
 	}
 
 	void UpdateText() {
-		float msec = _deltaTime * 1000.0f;
-		float fps = 1.0f / _deltaTime;
-		_fpsText.text = string.Format("FPS: {0:0.0} ms ({1:0.} fps)", msec, fps);
+		_fpsText.text = string.Format("FPS: {0:0.0} ms ({1:0.} fps)", (_time / (float)_frames) * 1000f, (float)_frames / _time);
 	}
 }
